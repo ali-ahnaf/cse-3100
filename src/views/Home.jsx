61899c502d21c90855440077760ac6300e3fb7d1
fuad <nafisfuadisc@gmail.com>
2025-12-18 00:03:44 +0600

    fix: grid view of featured cats

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,31 +5,27 @@ export default function Home() {
 
   return (
     <>
-      <section className="text-center mt-4">
-        <h2>Welcome to Purrfect Adoption</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
-        </p>
+      <section className="d-flex justify-content-center mt-4">
+        <div className="text-center" style={{ maxWidth: "720px" }}>
+          <h2>Welcome to Purrfect Adoption</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
+          </p>
+        </div>
       </section>
 
       <section className="mt-5">
         <h2>Featured cats</h2>
-        <div className="mt-2 row g-4" id="cats-container"></div>
         <div className="mt-2 row g-4" id="cats-container">
           {cats.map((cat, i) => (
-            <div key={i} className="col-md-4">
+            <div key={i} className="col-md-3">
               <div className="cat-card">
                 <img
                   src={cat.image}
                   alt={cat.name}
-                  className="img-fluid mb-2"
-                  style={{
-                    borderRadius: "8px",
-                    height: "200px",
-                    objectFit: "cover",
-                  }}
+                  className="img-fluid"
                 />
                 <div className="cat-info">
                   <h3 className="h5 mb-1">{cat.name}</h3>
