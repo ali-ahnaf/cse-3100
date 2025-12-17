10f3a955cf8daab6911537c4b354bd79d239fde5
fuad <nafisfuadisc@gmail.com>
2025-12-17 22:49:13 +0600

    fix: resolved footer positioning

--- src/views/BaseLayout.jsx
+++ src/views/BaseLayout.jsx
@@ -3,28 +3,30 @@ import { Link, Outlet } from "react-router-dom";
 const BaseLayout = () => {
   return (
     <div className="layout">
-      <header className="d-flex align-items-center bg-light">
-        <h1>
-          <Link className="text-decoration-none text-dark" to="/">
-            Purrfect Adoption
-          </Link>
-        </h1>
-        <div className="flex-grow-1"></div>
-        <nav>
-          <ul className="nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/available-cats">
-                Available Cats
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main id="content">
-        <Outlet />
-      </main>
+      <div>
+        <header className="d-flex justify-content-between align-items-center bg-light">
+          <h1>
+            <Link className="text-decoration-none text-dark" to="/">
+              Purrfect Adoption
+            </Link>
+          </h1>
+          <nav>
+            <ul className="nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/available-cats">
+                  Available Cats
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main id="content">
+          <Outlet />
+        </main>
+      </div>
+
       <footer className="bg-light">
-        <p>© Copyright 2024</p>
+        <p>© Copyright {new Date().getFullYear()}</p>
       </footer>
     </div>
   );
