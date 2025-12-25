c2cf879c4e5b69c6ade8abac23a669a00b95b2c4
Hrittika Saha <68088694+hrittikaaa@users.noreply.github.com>
2025-12-25 23:30:40 +0600

    Some UI updates.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
       try {
         const responses = await Promise.all(
           featuredCats.map(() =>
-            fetch("https://api.thecatapi.com/v1/images/search?limit=10").then(
+            fetch("https://api.thecatapi.com/v1/images/search").then(
               (res) => res.json()
             )
           )
@@ -49,42 +49,45 @@ export default function Home() {
   }, []); //Added dependency array to stop the infinite render
 
   return (
-    <>
+    <div className="mx-auto" style={{ width: "75%" }}>
       <section className="text-center mt-4">
         <h2 className="fw-bold">Welcome to Purrfect Adoption</h2>
-        <p className="p-4 mx-auto text-center" style={{ width: "70%" }}>
-          Welcome to <b>Purrfect Adoptions!</b> We're dedicated to finding loving homes
-          for cats in need. Every cat deserves a second chance at happiness, and
-          we're here to make that connection between you and your future feline
-          friend. Browse our available cats, learn about the adoption process,
-          and discover how you can make a difference in a cat's life today.
+        <p className="p-4">
+          Welcome to <b>Purrfect Adoptions!</b> We're dedicated to finding
+          loving homes for cats in need. Every cat deserves a second chance at
+          happiness, and we're here to make that connection between you and your
+          future feline friend. Browse our available cats, learn about the
+          adoption process, and discover how you can make a difference in a
+          cat's life today.
         </p>
       </section>
 
       <section className="m-4">
-        <h2>Featured cats</h2>
-        <div className="mt-2 row g-4" id="cats-container">
+        <h2 className="fw-bold">Featured cats</h2>
+        <div className="mt-2 row g-3" id="cats-container">
           {cats.map((cat, i) => (
-            <div key={i} className="col-md-4 col-lg-2">
+            <div key={i} className="col-12 col-sm-6 col-md-2">
               <div className="cat-card">
                 <img
                   src={cat.image}
                   alt={cat.name}
                   className="img-fluid"
-                  style={{
-                    objectFit: "cover",
-                  }}
+                  style={{ objectFit: "cover" }}
                 />
                 <div className="cat-info py-2">
                   <h3 className="mb-1">{cat.name}</h3>
-                  <p className="mb-0"><b>Breed:</b> {cat.breed}</p>
-                  <p className="mb-0"><b>Age:</b> {cat.age}</p>
+                  <p className="mb-0">
+                    <b>Breed:</b> {cat.breed}
+                  </p>
+                  <p className="mb-0">
+                    <b>Age:</b> {cat.age}
+                  </p>
                 </div>
               </div>
             </div>
           ))}
         </div>
       </section>
-    </>
+    </div>
   );
 }
