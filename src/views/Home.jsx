40457a724e0e5fd44a1fe7c5a2904c30b9c65a77
abid <gisankhan299@gmail.com>
2025-12-25 19:26:36 +0600

    Fixed according to the assignment requirements

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,13 @@ const featuredCats = [
   { name: 'Whiskers', age: '2' },
   { name: 'Mittens', age: '2' },
   { name: 'Shadow', age: '1' },
+  { name: 'Pumpkin', age: '3' },
+  { name: 'Luna', age: '4' },
+  { name: 'Simba', age: '2' },
+
+  // ✅ Added 2 extra cats
+  { name: 'Oliver', age: '3' },
+  { name: 'Nala', age: '1' },
 ];
 
 export default function Home() {
@@ -38,23 +45,21 @@ export default function Home() {
     };
 
     fetchCatImages();
-  });
+  }, []);
 
   return (
     <>
       <section className="text-center mt-4">
         <h2>Welcome to Purrfect Adoption</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
+          Our TAI LUNG team will help you find your perfect furry friend.
+          They are waiting to meet you!
         </p>
       </section>
 
       <section className="mt-5">
-        <h2>Featured cats</h2>
-        <div className="mt-2 row g-4" id="cats-container"></div>
-        <div className="mt-2 row g-4" id="cats-container">
+        <h2 style={{ textAlign: 'left' }}>Featured cats</h2>
+        <div className="mt-2 row g-4 cats-container" id="cats-container">
           {cats.map((cat, i) => (
             <div key={i} className="col-md-4">
               <div className="cat-card">
@@ -64,7 +69,6 @@ export default function Home() {
                   className="img-fluid mb-2"
                   style={{
                     borderRadius: '8px',
-                    height: '200px',
                     objectFit: 'cover',
                   }}
                 />
