7bd7f66157cb969c5834171a7e946c52275b5168
Md. Fazle Rabbi <fazle.rabbi.mugdho@gmail.com>
2025-12-27 02:14:49 +0600

    Fixed CSS grid layout for cat cards, Fixed infinite state loop bug in Home and, AvailableCats, Added breed field to cats, Implemented breed dropdown filter and name search functionality, Created Contact Us and About Us Donate pages

--- src/views/BaseLayout.jsx
+++ src/views/BaseLayout.jsx
@@ -3,28 +3,25 @@ import { Link, Outlet } from 'react-router-dom';
 const BaseLayout = () => {
   return (
     <div className="layout">
-      <header className="d-flex align-items-center bg-light">
-        <h1>
-          <Link className="text-decoration-none text-dark" to="/">
-            Purrfect Adoption
-          </Link>
-        </h1>
-        <div className="flex-grow-1"></div>
-        <nav>
-          <ul className="nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/available-cats">
-                Available Cats
-              </Link>
-            </li>
-          </ul>
+      <header className="header-horizontal-blue">
+        <div className="brand-logo">
+          <Link to="/">Purrfect Adoption</Link>
+        </div>
+        
+        <nav className="nav-links-container">
+          <Link className="nav-item-link" to="/about">About us</Link>
+          <Link className="nav-item-link" to="/available-cats">Available cats</Link>
+          <Link className="nav-item-link" to="/donate">Donate</Link>
+          <Link className="nav-item-link" to="/contact">Contact us</Link>
         </nav>
       </header>
+
       <main id="content">
         <Outlet />
       </main>
-      <footer className="bg-light">
-        <p>© Copyright 2024</p>
+
+      <footer className="footer-blue">
+        <p>© Copyright 2024 - Purrfect Adoption</p>
       </footer>
     </div>
   );
