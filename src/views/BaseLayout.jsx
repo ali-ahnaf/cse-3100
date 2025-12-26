5b9fafa5c7635d7eed83141ea02ac7b46f9ae48c
Samanta503 <samanta503@users.noreply.github.com>
2025-12-26 15:37:41 +0600

    Fix CSS layout, navbar, infinite loop, and match mockup design

--- src/views/BaseLayout.jsx
+++ src/views/BaseLayout.jsx
@@ -3,27 +3,23 @@ import { Link, Outlet } from 'react-router-dom';
 const BaseLayout = () => {
   return (
     <div className="layout">
-      <header className="d-flex align-items-center bg-light">
-        <h1>
-          <Link className="text-decoration-none text-dark" to="/">
-            Purrfect Adoption
-          </Link>
+      <header className="navbar">
+        <h1 className="navbar-brand">
+          <Link to="/">Purrfect Adoption</Link>
         </h1>
-        <div className="flex-grow-1"></div>
-        <nav>
-          <ul className="nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/available-cats">
-                Available Cats
-              </Link>
-            </li>
-          </ul>
+        <nav className="navbar-nav">
+          <Link className="nav-link" to="/available-cats">
+            Available cats
+          </Link>
+          <Link className="nav-link" to="/donate">
+            Donate
+          </Link>
         </nav>
       </header>
       <main id="content">
         <Outlet />
       </main>
-      <footer className="bg-light">
+      <footer>
         <p>© Copyright 2024</p>
       </footer>
     </div>
