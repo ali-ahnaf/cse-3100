29e1e533a0983178edaf062ddf6c7fa363193ead
fuad <nafisfuadisc@gmail.com>
2025-12-26 19:45:40 +0600

    fix: resolved cats grid gap

--- src/components/GridCats.jsx
+++ src/components/GridCats.jsx
@@ -12,15 +12,17 @@ GridCats.Title = ({ children }) => {
 
 GridCats.Content = ({ cats }) => {
   return (
-    <div className="row g-4">
+    <div className="row">
       {cats.map((cat, i) => (
-        <div key={i} className="col-md-3">
-          <div className="cat-card">
-            <img src={cat.image} alt={cat.name} className="img-fluid" />
-            <div className="cat-info">
-              <div className="info-box">
-                <div className="fw-bold">{cat.name}</div>
-                <small>Age: {cat.age}</small>
+        <div key={i} className="col-md-3 px-0">
+          <div className="px-2">
+            <div className="cat-card">
+              <img src={cat.image} alt={cat.name} className="img-fluid" />
+              <div className="cat-info">
+                <div className="info-box">
+                  <div className="fw-bold">{cat.name}</div>
+                  <small>Age: {cat.age}</small>
+                </div>
               </div>
             </div>
           </div>
