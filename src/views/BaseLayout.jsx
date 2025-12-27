8ac48f46d99e2d2d3b5b6b4fcb65d04f606f248f
Fahim <fahimimam786@gmail.com>
2025-12-27 19:32:07 +0600

    Changing the header and navigation

--- src/views/BaseLayout.jsx
+++ src/views/BaseLayout.jsx
@@ -3,22 +3,23 @@ import { Link, Outlet } from 'react-router-dom';
 const BaseLayout = () => {
   return (
     <div className="layout">
-      <header className="d-flex align-items-center bg-light">
-        <h1>
-          <Link className="text-decoration-none text-dark" to="/">
-            Purrfect Adoption
-          </Link>
-        </h1>
-        <div className="flex-grow-1"></div>
-        <nav>
-          <ul className="nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/available-cats">
-                Available Cats
-              </Link>
-            </li>
-          </ul>
-        </nav>
+      <header className="site-header">
+        <div className="header-inner">
+          <h1 className="site-brand">
+            <Link className="site-brand-link" to="/">
+              Purrfect Adoption
+            </Link>
+          </h1>
+          <nav className="site-nav" aria-label="Main navigation">
+            <ul className="site-nav-list">
+              <li>
+                <Link className="site-nav-link" to="/available-cats">
+                  Available Cats
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <main id="content">
         <Outlet />
