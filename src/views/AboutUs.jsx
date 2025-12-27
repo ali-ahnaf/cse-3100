5eda21cd0044831bae480ebb4233b39013c2085e
samiarpita <arpitarahmansamia@gmail.com>
2025-12-27 12:29:33 +0600

    bug fixed and added filters

---
+++ src/views/AboutUs.jsx
@@ -0,0 +1,21 @@
+export default function AboutUs(){
+    return(
+        <section className="mt-4">
+            <h2>About Us</h2>
+            <p>
+                Purrfect Adoption is a platform dedicated to helping cats find loving
+                and permanent homes. We connect adopters with adorable cats in need of
+                care and affection.
+            </p>
+            <h2>Our Mission</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+               Maecenas luc Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Maecenas luc Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
+              </p>
+
+              <h2>Our History</h2>
+              <p>Founded in 2010, Purrfect Adoption began as a small initiative to help stray cats find homes.
+                 Over the years, we've grown into a trusted platform connecting thousands of cats with loving families.</p>
+        </section>
+    );
+}
---
+++ src/views/ContactUs.jsx
@@ -0,0 +1,20 @@
+export default function ContactUs(){
+    return(
+        <section className="mt-4">
+            <h2>Contact Us</h2>
+            <p>
+                We would love to hear from you! Whether you have questions about our adoption process,
+                want to share feedback, or need assistance, feel free to reach out to us.
+            </p>
+            <div className="mt-3">
+                <h3>Email</h3>
+                <p>Email:purrfect@gmail.com</p>
+            </div>
+            <div className="mt-3">
+                <h3>Phone</h3>
+                <p>Phone:+880136589745</p>
+            </div>
+            </section>
+    );
+}
+
