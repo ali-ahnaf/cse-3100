4d31d4df60d48ef84860cac562c9c68c6324d731
Shahadat Hasan <tamimshahadat15@gmail.com>
2025-12-27 20:20:48 +0600

    Fixed the State & Routing

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       try {
         const responses = await Promise.all(
           featuredCats.map(() =>
-            fetch('https://api.thecatapi.com/v1/images/search').then((res) =>
+            fetch('https://api.thecatapi.com/v1/images/search').then(res =>
               res.json()
             )
           )
@@ -25,20 +25,21 @@ export default function Home() {
           image: responses[index][0].url,
         }));
 
-        setCats((prevCats) => [...prevCats, ...catsWithImages]);
-
-        if (cats.length > 10) {
-          alert(
-            'Hey, you should quickly fix this infinite state loop before your PC crashes! Stop the App, Refresh the browser and fix the bug!! '
-          );
-        }
+        /* FIX 1:
+           Replace state instead of appending.
+           Appending causes infinite growth */
+        setCats(catsWithImages);
       } catch (error) {
         console.error('Error fetching cat images:', error);
       }
     };
 
     fetchCatImages();
-  });
+
+    /* FIX 2:
+       Empty dependency array ensures this runs
+       only once on component mount */
+  }, []);
 
   return (
     <>
@@ -46,15 +47,12 @@ export default function Home() {
         <h2>Welcome to Purrfect Adoption</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
         </p>
       </section>
 
       <section className="mt-5">
         <h2>Featured cats</h2>
-        <div className="mt-2 row g-4" id="cats-container"></div>
-        <div className="mt-2 row g-4" id="cats-container">
+        <div className="mt-2 row g-4">
           {cats.map((cat, i) => (
             <div key={i} className="col-md-4">
               <div className="cat-card">
