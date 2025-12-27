85875d7f8f1659d92e6eb2da3da3090fc5dc2407
fuad <nafisfuadisc@gmail.com>
2025-12-27 14:07:05 +0600

    fix: form input field width

--- src/views/ContactUs.jsx
+++ src/views/ContactUs.jsx
@@ -28,17 +28,17 @@ export default function ContactUs() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc
       </p>
-      <form onSubmit={submit}>
+      <form onSubmit={submit} className="my-form">
         <label className="pb-1" htmlFor="name">Name</label>
-        <input type="text" className="form-control" autoComplete="true" style={{ width: "90%" }}
+        <input type="text" className="form-control" autoComplete="true"
         onChange={handleFormText} id="name" name="name" placeholder="Nafis Fuad" value={formText.name} />
 
         <label className="pt-2 pb-1" htmlFor="phone">Phone</label>
-        <input type="tel" className="form-control" autoComplete="true" pattern="\+880 1\d{3}-\d{6}" style={{ width: "90%" }}
+        <input type="tel" className="form-control" autoComplete="true" pattern="\+880 1\d{3}-\d{6}"
         onChange={handleFormText} id="phone" name="phone" placeholder="+880 1604-3XXXXX" value={formText.phone} />
 
         <label className="pt-2 pb-1" htmlFor="email">Email</label>
-        <input type="text" className="form-control" autoComplete="true" style={{ width: "90%" }}
+        <input type="text" className="form-control" autoComplete="true"
         onChange={handleFormText} id="email" name="email" placeholder="nafis.ceo@purrfect.com" value={formText.email} />
 
         <button type="submit" className="btn btn-success mt-3">
