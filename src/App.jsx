3649f81f442b1e1d15983920d3065d0c1eeed32e
rupamimamhasan-86 <rupamimamhasan@gmail.com>
2025-12-28 19:00:57 +0600

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Outlet, Route, Routes } from 'react-router';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import BaseLayout from './views/BaseLayout';
 import Home from './views/Home';
 import AvailableCats from './views/AvailableCats';
-import About from './views/About';     // <--- Added This
-import Contact from './views/Contact'; // <--- Added This
+import About from './views/About';
+import Contact from './views/Contact';
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Route element={<BaseLayout><Outlet /></BaseLayout>}>
         <Route path="/" element={<Home />} />
         <Route path="/available-cats" element={<AvailableCats />} />
-        <Route path="/about" element={<About />} />      {/* <--- Added This */}
-        <Route path="/contact" element={<Contact />} />  {/* <--- Added This */}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
     </Routes>
   );
