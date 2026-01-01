a222225ae2dba2da0a0d54954b111c4b3fa52245
abonty360 <143591727+abonty360@users.noreply.github.com>
2026-01-02 00:43:37 +0600

    Added Breed type

--- src/views/AvailableCats.jsx
+++ src/views/AvailableCats.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react';
 
 const availableCats = [
-  { name: 'Whiskers', age: '2' },
-  { name: 'Mittens', age: '2' },
-  { name: 'Shadow', age: '1' },
-  { name: 'Pumpkin', age: '3' },
-  { name: 'Luna', age: '4' },
-  { name: 'Simba', age: '2' },
+  { name: 'Whiskers', age: '2', breed: 'Sphynx' },
+  { name: 'Mittens', age: '2', breed: 'Peterbald' },
+  { name: 'Shadow', age: '1', breed: 'Birman' },
+  { name: 'Pumpkin', age: '3', breed: 'Abyssinian' },
+  { name: 'Luna', age: '4', breed: 'Persian' },
+  { name: 'Simba', age: '2', breed: 'Bengal' },
 ];
 
 export default function AvailableCats() {
   const [cats, setCats] = useState([]);
+
+  const [breedInsert, setBreedInsert] = useState("");
+  const [nameInsert, setNameInsert] = useState("");
+  const [selectedBreedFilter, setSelectedBreedFilter] = useState("");
+  const [selectedNameFilter, setselectedNameFilter] = useState("");
 
   useEffect(() => {
     const fetchCatImages = async () => {
@@ -36,18 +41,78 @@ export default function AvailableCats() {
     fetchCatImages();
   }, []);
 
+  const handleSearch = () => {
+    setSelectedBreedFilter(breedInsert);
+    setselectedNameFilter(nameInsert);
+  };
+
+  const filteredResults = cats.filter((cat) => {
+    const matchName = cat.name.toLowerCase().includes(selectedNameFilter.toLowerCase());
+    const matchBreed = selectedBreedFilter === "" || cat.breed === selectedBreedFilter;
+    return matchName && matchBreed;
+  });
+
+  const breeds = [...new Set(availableCats.map(cat => cat.breed))];
+
   return (
     <section className="text-center mt-4">
-       <div style={{ display: 'flex', justifyContent: 'space-between',  marginBottom: '5px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '5px' }}>
 
-        <h2 className="" style={{textAlign: 'left'}}>Available cats</h2>
+        <h2 className="" style={{ textAlign: 'left' }}>Available cats</h2>
         <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-          </div>
-          </div>
+
+          <select
+            className="form-select"
+            style={{
+              width: "180px",
+              padding: "5px",
+              borderRadius: "5px",
+              border: "1px solid #333"
+            }}
+            value={breedInsert}
+            onChange={(e) => setBreedInsert(e.target.value)}
+          >
+            <option value="">Select breed</option>
+            {breeds.map((breed, index) => (
+              <option key={index} value={breed}>{breed}</option>
+            ))}
+          </select>
+
+          <input
+            type="text"
+            placeholder="Search by name"
+            className="form-control"
+            style={{
+              width: "190px",
+              padding: "7px",
+              borderRadius: "5px",
+              border: "1px solid #333"
+            }}
+            value={nameInsert}
+            onChange={(e) => setNameInsert(e.target.value)}
+          />
+
+          <button
+            onClick={handleSearch}
+            style={{
+              backgroundColor: '#90CDF4',
+              border: '1px solid #333',
+              borderRadius: '5px',
+              padding: '5px 20px',
+              cursor: 'pointer',
+              fontWeight: 'bold',
+              fontFamily: 'inherit'
+            }}
+          >
+            search
+          </button>
+
+        </div>
+      </div>
       <p>Meet our adorable cats looking for their forever home!</p>
-     <hr style={{ border: 'none', borderTop: '2px solid #333', opacity: 1, margin: '0 0 30px 0' }} />
+      <hr style={{ border: 'none', borderTop: '2px solid #333', opacity: 1, margin: '0 0 30px 0' }} />
       <div className="mt-2 row g-4 cats-container" id="cats-container">
-        {cats.map((cat, i) => (
+        {filteredResults.map((cat, i) => (
           <div key={i} className="col-md-4">
             <div className="cat-card">
               <img
@@ -56,7 +121,6 @@ export default function AvailableCats() {
                 className="img-fluid mb-2"
                 style={{
                   borderRadius: '8px',
-                  height: '200px',
                   objectFit: 'cover',
                 }}
               />
