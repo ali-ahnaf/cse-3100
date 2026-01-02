8a505865ee6609ee4f120ab3ebcb55a9a3ce756c
Zumaina <zumainatahsincat@gmail.com>
2026-01-02 14:14:31 +0600

    fix: add missing navigation links

--- src/views/BaseLayout.jsx
+++ src/views/BaseLayout.jsx
@@ -13,8 +13,28 @@ const BaseLayout = () => {
         <nav>
           <ul className="nav">
             <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
               <Link className="nav-link" to="/available-cats">
                 Available Cats
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/donate">
+                Donate
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About Us
               </Link>
             </li>
           </ul>
