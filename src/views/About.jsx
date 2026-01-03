c6908e3703a160ae1dc51ffe433038c7c4a63054
tasmiatabassum302 <tasmia.cse.20230104139@aust.edu>
2026-01-03 22:04:06 +0600

    added dropdown to filter cats by breeds

---
+++ src/views/About.jsx
@@ -0,0 +1,17 @@
+
+
+export default function About() {
+  return (
+    <section className="section">
+      <h2>Our Mission</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc lorem ipsum dolor sit amet, consectetur adipiscing elit.Maecenas luc
+</p>
+      
+      <h2>Our History</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luc lorem ipsum dolor sit amet, consectetur adipiscing elit.Maecenas luc </p>
+
+
+      
+    </section>
+  );
+}
