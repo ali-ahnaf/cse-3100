7475550b15ada562a31aec753e9d613316d63287
Masrafi Iqbal <masrafiiqbal@gmail.com>
2026-01-03 11:53:45 +0600

    Assignment 1  20230104141 fixed

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -73,6 +73,7 @@ export default function Home() {
                 <div className="cat-info">
                   <h3 className="h5 mb-1">{cat.name}</h3>
                   <p className="mb-0">Age: {cat.age}</p>
+                  <p className="mb-0">Breed: {cat.breed}</p>
                 </div>
               </div>
             </div>
