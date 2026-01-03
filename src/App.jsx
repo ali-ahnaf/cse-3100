88b6ff1059e0842a384f0a709e835ce8247e327e
Masrafi Iqbal <masrafiiqbal@gmail.com>
2026-01-03 11:08:12 +0600

    completed assignment 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { Outlet, Route, Routes } from 'react-router';
-import BaseLayout from './views/BaseLayout';
-import Home from './views/Home';
-import AvailableCats from './views/AvailableCats';
+import { Route, Routes } from "react-router-dom";
+import BaseLayout from "./views/BaseLayout";
+import Home from "./views/Home";
+import ContactUs from "./views/Contactus";
+import AboutUs from "./views/Aboutus";
+import Donate from "./views/Donate";
+import AvailableCats from "./views/AvailableCats";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        element={
-          <BaseLayout>
-            <Outlet />
-          </BaseLayout>
-        }
-      >
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/available-cats'} element={<AvailableCats />} />
-      </Route>
-    </Routes>
+    
+      <Routes>
+        <Route element={<BaseLayout />}>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/available-cats"} element={<AvailableCats />} />
+          <Route path={"/about-us"} element={<AboutUs />} />
+          <Route path={"/contact-us"} element={<ContactUs />} />
+          <Route path={"/donate"} element={<Donate />} />
+        </Route>
+      </Routes>
+
   );
 }
 
